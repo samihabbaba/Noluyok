1e8c566bce70b23f7b7974b6b8045ebe48324445
sammy2h <70750616+sammy2h@users.noreply.github.com>
2021-09-01 00:07:48 +0300

    sum changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductCardComponent } from './shared/product-card/product-card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProductCardComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+
+  products: any[] = [{
+    id: '1',title: 'Adidas SweatShirt', price: 200, discountPrice: 125
+  }]
 
   constructor() { }
 }
